fc28933538157fd3042bc39e2aecb6e6d06f0756
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-01-02 19:55:17 +0200

    firstseen

--- main/commands/log/firstseen.js
+++ main/commands/log/firstseen.js
@@ -15,11 +15,19 @@ module.exports.run = (channel, userstate, params) => {
       if (short[userLow][1].length < 1) { // no logs but is tracked somehow
         return resolve(`${self ? `@${userstate['display-name']} You` : params[1]} ${youOrMe(self)}`)
       }
-      let seenMs = nmb.logger.getTime(short, userLow, short[userLow][2].length - (self ? 1 : 0)) * 1000
-
-      resolve(`${self ? `@${userstate['display-name']} You` : params[1]} ${youOrMe2(self)} ${myUtil.timeSince(seenMs, 1, false)} ago`)
+      let seenMs = nmb.logger.getTime(short, userLow, 1) * 1000
+      resolve(`${self ? `@${userstate['display-name']} You` : params[1]} ${youOrMe2(self)} on ${myUtil.dateString(seenMs)} ${nearTrackStart(seenMs, channel)}`)
     } else resolve(`${self ? `@${userstate['display-name']} You` : params[1]} ${youOrMe(self)}`)
   })
+}
+
+// Returns a formatted string or '' depending on how close the dates are
+function nearTrackStart (compareMS, channel) {
+  let diff = compareMS - (nmb.bot.log[channel].start_time * 1000)
+  let maxDiff = 1209600000 // 14 days
+  if (diff < maxDiff) {
+    return `. (${myUtil.durationStr(diff, 1, false)} after tracking started)`
+  } return ''
 }
 
 // lidl functions
@@ -28,12 +36,12 @@ function youOrMe (me) {
   else return 'has not been tracked here before'
 }
 function youOrMe2 (me) {
-  if (me) return 'were last seen here'
-  else return 'was last seen here'
+  if (me) return 'were first seen here'
+  else return 'was first seen here'
 }
 
 module.exports.help = (params, channel) => {
   return new Promise((resolve, reject) => {
-    resolve(`Get when a user was last seen: ${params[1]} [<user>]. Logging for this channel started ${myUtil.dateString(nmb.bot.log[channel].start_time * 1000)}`)
+    resolve(`Get when a user was first seen: ${params[1]} [<user>]. Logging for this channel started ${myUtil.dateString(nmb.bot.log[channel].start_time * 1000)}`)
   })
 }
