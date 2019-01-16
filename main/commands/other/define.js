0141e9cb7bf17478fbe67823b5a4e499a1560b05
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-01-16 22:45:27 +0200

    Remove caching
    Good boy removes caching that violates Oxford Dictionary terms of service
    
    
    Former-commit-id: f4ff9e3b081f00d54b23f87a09cbbea2884c9a73

--- main/commands/other/define.js
+++ main/commands/other/define.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const util = require('util')
 var https = require('https')
 
-let dict = require('../../../data/global/oxDictionary.json')
 const myUtil = require('../../myutil')
 const app = require('../../../keyConfig/OxfordDictionaries.json')
 
@@ -14,43 +13,37 @@ module.exports.run = (channel, userstate, params) => {
 
     let words = params.slice(1).join(' ')
     if (params[1].toLowerCase() === 'shushama') return resolve('[Noun /shüseimm/ Shushama]: A person who steals another person\'s property, especially by stealth, using force or threat of violence. Not ninja. So weeb.⁭ ')
-    if (words in dict) { // Find if in cache
-      console.log(`* [${channel}] Using cached definition of "${words}"`)
-      return resolve(`[${dict[words].cat} ${dict[words].pronun}${dict[words].word}]: ${dict[words].definition}${dict[words].definition.endsWith('.') ? '' : '.'}`)
-    } else {
-      define(words, lang, (error, data) => {
-        if (error) return console.error(error)
-        else {
-          // console.log(util.inspect(data, { showHidden: false, depth: null }))
-          if (typeof data.results === 'undefined' || typeof data === 'undefined') {
-            return resolve(data)
-          }
-          if ((((((((data.results[0].lexicalEntries || {})[0] || {}).entries || {})[0] || {}).senses || {})[0] || {}).definitions || {})[0]) var definition = data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
-          else definition = 'No definition.'
-          let word = data.results[0].word
-          if (definition.charAt(0) === '(') { // Capitalize char after paren
-            definition = myUtil.cap(definition, 1)
-          } else { // Capitalize first char
-            definition = myUtil.cap(definition)
-          }
-          let pronun // pronunciation
-          let cat // noun, verb etc
-          if (((((data.results[0].lexicalEntries || {})[0] || {}).pronunciations || {})[0] || {}).phoneticSpelling) {
-            pronun = '/' + data.results[0].lexicalEntries[0].pronunciations[0].phoneticSpelling + '/ '
-          } else pronun = ''
 
-          if (((data.results[0].lexicalEntries || {})[0] || {}).lexicalCategory) {
-            cat = data.results[0].lexicalEntries[0].lexicalCategory // noun, verb etc
-          } else cat = 'Unknown?'
-          // save to cache
-          dict[words] = { 'cat': cat, 'pronun': pronun, 'word': word, 'definition': definition }
-          console.log(`* [GLOBAL] Cached definition of "${words}"`)
-          save(dict)
-
-          resolve(`[${cat} ${pronun}${word}]: ${definition}${definition.endsWith('.') ? '' : '.'}`)
+    define(words, lang, (error, data) => {
+      if (error) return console.error(error)
+      else {
+        // console.log(util.inspect(data, { showHidden: false, depth: null }))
+        if (typeof data.results === 'undefined' || typeof data === 'undefined') {
+          return resolve(data)
         }
-      })
-    }
+        if ((((((((data.results[0].lexicalEntries || {})[0] || {}).entries || {})[0] || {}).senses || {})[0] || {}).definitions || {})[0]) var definition = data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+        else definition = 'No definition.'
+        let word = data.results[0].word
+        if (definition.charAt(0) === '(') { // Capitalize char after paren
+          definition = myUtil.cap(definition, 1)
+        } else { // Capitalize first char
+          definition = myUtil.cap(definition)
+        }
+        let pronun // pronunciation
+        let cat // noun, verb etc
+        if (((((data.results[0].lexicalEntries || {})[0] || {}).pronunciations || {})[0] || {}).phoneticSpelling) {
+          pronun = '/' + data.results[0].lexicalEntries[0].pronunciations[0].phoneticSpelling + '/ '
+        } else pronun = ''
+
+        if (((data.results[0].lexicalEntries || {})[0] || {}).lexicalCategory) {
+          cat = data.results[0].lexicalEntries[0].lexicalCategory // noun, verb etc
+        } else cat = 'Unknown?'
+        // save to cache
+        console.log(`* [GLOBAL] Cached definition of "${words}"`)
+
+        resolve(`[${cat} ${pronun}${word}]: ${definition}${definition.endsWith('.') ? '' : '.'}`)
+      }
+    })
 
     function define (words, lang, cb) {
       var options = {
@@ -102,16 +95,6 @@ module.exports.run = (channel, userstate, params) => {
           default:
             cb(null, `Something odd happened o_O`)
             break
-        }
-      })
-    }
-
-    function save (dict) {
-      fs.writeFile('./data/global/oxDictionary.json', JSON.stringify(dict, null, 2), (err) => {
-        if (!err) {
-          console.log(`* [GLOBAL] Modified dictionary file`)
-        } else {
-          console.error(`* [GLOBAL] FAILED TO MODIFY DICTIONARY FILE: ${err}`)
         }
       })
     }
