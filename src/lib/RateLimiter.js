afeebaf4ec297316f376f70b16b93113313d2e1a
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-02-16 04:19:54 +0200

    -
    Former-commit-id: 08db7fb97ca2383f2a92397694798f9eabea925d

--- src/lib/RateLimiter.js
+++ src/lib/RateLimiter.js
@@ -3,31 +3,31 @@
  * Enables creating ratelimits for actions  
  * 
  */
-module.exports = class RateHandler {
+module.exports = class RateLimiter {
   /**
-   * Constructor a new RateHandler  
+   * Construct a new RateLimiter  
    * @param {Object} options
    * @param {number} options.duration Maximum age of an entry
    * @param {number} options.limit Maximum entries per duration
    * @param {number} options.delay Minimum time between entries
-   * @param options.times Reference to an array or property. Using a non referencing value has unknown implications
+   * @param {array} options.times Reference to an array
    */
   constructor (options = {}) {
     this.duration = 60000
     this.limit = 10
     this.delay = 0
-    this._times = []
+    this.times = []
 
     this.set(options)
   }
 
   /**
-   * Edit a bunch of options
+   * Edit variables in object format
    * @param {Object} options 
    * @param {number} options.duration Maximum age of an entry
    * @param {number} options.limit Maximum entries per duration
    * @param {number} options.delay Minimum time between entries
-   * @param options.times Reference to an array or property. Using a non referencing value has unknown implications
+   * @param {array} options.times Reference to an array
    */
   set (options = {}) {
     if (options.duration) this.duration = options.duration
@@ -36,20 +36,8 @@ module.exports = class RateHandler {
     if (options.times) this.times = options.times
   }
 
-  set times (v) {
-    // move existing array contents to the new array
-
-    // !!!ASSIGNMENT TO _times WORKS BUT NOT WITH THIS SETTER
-
-    v = this._times.slice()
-    this._times = v
-  }
-  get times () {
-    return this._times
-  }
-
   add (ms = Date.now()) {
-    this._times.push(ms)
+    this.times.push(ms)
   }
 
   /**
@@ -60,22 +48,26 @@ module.exports = class RateHandler {
     let now = Date.now()
 
     // Remove times older than duration
-    for (let i = 0; i < this._times.length; i++) {
-      if (this._times[i] < now - this.duration) { // time is expired
-        this._times.shift()
+    for (let i = 0; i < this.times.length; i++) {
+      if (this.times[i] < now - this.duration) { // time is expired
+        this.times.shift()
         i--
       } else break
     }
 
     // Calculate next time
-    if (this._times.length < this.limit) { // Limit is not reached
+    if (this.times.length < this.limit) { // Limit is not reached
       // Calculate needed wait for delay
-      return this._times.length ? (this._times[this._times.length - 1] + this.delay) - now : -1
+      return this.times.length ? (this.times[this.times.length - 1] + this.delay) - now : 0
     } else {
-      let exceeds = this._times.length - this.limit
-      let delayTest = (this._times[this._times.length - 1] + this.delay) - now // test only for delay
-      let limitTest = (this._times[0 + exceeds] + this.duration) - now // test all but delay
+      let exceeds = this.times.length - this.limit
+      let delayTest = (this.times[this.times.length - 1] + this.delay) - now // test only for delay
+      let limitTest = (this.times[0 + exceeds] + this.duration) - now // test all but delay
       return delayTest > limitTest ? delayTest : limitTest
     }
+  }
+
+  remaining () {
+    return this.limit - this.times.length
   }
 }
