bde46333a02fa1181e6fb775a771d5739d315fbe
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-02-26 14:29:42 +0200

    Update test.js
    Former-commit-id: a7e3ccc2362abb4baad84533a1d90d21830c4500

--- src/test.js
+++ src/test.js
@@ -1,14 +1,13 @@
 
-const keys = require('./lib/secretKey')
+const secretKey = require('./lib/secretKey')
 const Client = require('./lib/Client')
 var client = new Client({
-  username: keys.getKey('./src/cfg/keys.json', 'twitch', 'username'),
-  password: keys.getKey('./src/cfg/keys.json', 'twitch', 'password')
+  username: secretKey.getKey('./src/cfg/keys.json', 'twitch', 'username'),
+  password: secretKey.getKey('./src/cfg/keys.json', 'twitch', 'password')
 })
-global.keys = keys
+global.keys = secretKey
 global.client = client
 client.connect()
-console.time('abc')
-console.timeEnd('abc')
+
 setTimeout(() => {
 }, 10000000)
