0018c00b3907fa3aace273771e5e13f58e65c74c
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-02-28 15:38:03 +0200

    setKey

--- src/lib/secretKey.js
+++ src/lib/secretKey.js
@@ -4,11 +4,13 @@
 var cache = {}
 
 module.exports = {
+
   /**
    * Return key value if it exists in JSON `file`  
    * Creates `file` and the key if needed. Created keys are set to null
    * @param {string} file Path to file containing the wanted key
-   * @param {string} keys Path to a key. Like `f(<path>,'foo','bar')` = `file`.foo.bar  
+   * @param {...string} keys Path to a key.  
+   * Like f(file,`'foo','bar')` -> file.`foo.bar`
    * @Return Undefined if the key is created, otherwise the key's value
    */
   getKey: (file, ...keys) => {
@@ -44,6 +46,39 @@ module.exports = {
     }
     if (changed) fs.writeFileSync(file, JSON.stringify(cache[file], null, 2))
     return undefined
+  },
+
+  /**
+   * Set key value in JSON `file`  
+   * Creates `file` if needed
+   * @param {string} file Path to file containing the wanted key
+   * @param {...string} keysAndVal Path to a key.  
+   * Like f(file,`'foo','bar','value')` -> file.`foo.bar = 'value'`
+   * @Return Undefined if the key is created, otherwise the key's previous value
+   */
+  setKey: (file, ...keysAndVal) => {
+    if (!cache.file) {
+      fs.mkdirSync(file.replace(/\/[^/]*$/, ''), { recursive: true })
+      if (!fs.existsSync(file)) {
+        fs.writeFileSync(file, '{}')
+        cache[file] = {}
+      } cache[file] = JSON.parse(fs.readFileSync(file))
+    }
+    var keys = keysAndVal.splice(0, keysAndVal.length - 1)
+    var val = keysAndVal
+
+    var current = cache[file]
+    for (let i = 0; i < keys.length - 1; i++) {
+      if (typeof current[keys[i]] !== 'object' || current[keys[i]] === null) current[keys[i]] = {}
+      current = current[keys[i]]
+    }
+    var prevVal = current[keys[keys.length - 1]]
+    current[keys[keys.length - 1]] = val
+
+    console.log(keys, val, prevVal)
+    console.log(cache[file])
+
+    fs.writeFileSync(file, JSON.stringify(cache[file], null, 2))
   },
 
   /**
