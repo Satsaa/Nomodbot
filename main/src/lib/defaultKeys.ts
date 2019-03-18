41484218db4c4db3ae6b77bc8cec69d65434c18c
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-03-18 07:38:59 +0200

    Create defaultKeys.ts
    Former-commit-id: 35eccfe2abff4ef8404061a1ffd1c9cbfc2c5894

---
+++ main/src/lib/defaultKeys.ts
@@ -0,0 +1,36 @@
+
+/**
+ * Defines unexisting keys in `checkObj` that exist in `defaultObj`
+ * @param defaultObj 
+ * @param checkObj 
+ */
+export default (checkObj: object, defaultObj: object) => {
+
+  defaults(checkObj, defaultObj)
+
+  function defaults(checkObj: {[x: string]: any}, defaultObj: {[x: string]: any}) {
+
+    for (const key in defaultObj) { // loop all keys
+      if (isObject(defaultObj[key])) { // default key is object? Go deeper
+        if (!checkObj.hasOwnProperty(key)) { // Key doesnt exist?
+          checkObj[key] = Array.isArray(defaultObj[key]) ? [] : {} // Define as object type
+          defaults(checkObj[key], defaultObj[key]) // Go deeper
+        } else if (isObject(checkObj[key]) && Array.isArray(key) === Array.isArray(checkObj[key])) { // make sure both are same type of object
+          defaults(checkObj[key], defaultObj[key]) // Go deeper
+        } // continue if not same type of object
+      } else { // default key is not object. Set default value
+        if (!checkObj.hasOwnProperty(key)) { // Determine if the key exists
+          checkObj[key] = defaultObj[key] // Set to default value
+        }
+      }
+    }
+  }
+}
+
+function isObject(v: any) {
+  return v !== null && typeof v === 'object'
+}
+
+setInterval(() => {
+
+}, 100000000)
