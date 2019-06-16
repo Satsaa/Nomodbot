6bc55c75eeb73d04f549327da23f7395c8a9efc5
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-06-16 05:11:08 +0300

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,9 @@
 import Bot from './Bot'
 
-if (!process.send) {
-  // Managed instance. Debugging is not active
+const bot = new Bot({masters: [61365582]})
+if (!process.send) { // Managed instance? Debugging is not active
   // Pass reference to Bot for debugging
-  console.log(new Bot({masters: [61365582]}))
+  console.log(bot)
 }
 
 process.on('multipleResolves', (e, p, v) => { throw new Error(`Mutiple ${e}s\nvalue: ${v}`) })
