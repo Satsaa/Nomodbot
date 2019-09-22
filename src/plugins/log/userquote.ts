e5454644594566e9861aa99e05359c0f82900a7a
Samuel <32210043+Satsaa@users.noreply.github.com>
2019-09-22 17:09:47 +0300

    Update userquote.ts

--- src/plugins/log/userquote.ts
+++ src/plugins/log/userquote.ts
@@ -36,21 +36,6 @@ export class Instance implements PluginInstance {
     this.handlers = this.l.addHandlers(this, this.handlers, 'default', '[<USER>] [<INDEX>]', this.callUser)
   }
 
-  public async callRandom(channelId: number, userId: number, params: any, extra: Extra) {
-    const []: [] = params
-    const targetId = userId
-    let index
-
-    if (!index) index = this.l.u.randomInt(0, this.log.eventCount(channelId, userId, 'chat') || 0)
-
-    const res = await this.log.getSmartEvent(channelId, targetId, 'chat', index)
-    if (!res) return this.l.insertAtUser('Selected user had no messages', extra)
-    if (res.type === events.chat) {
-      return `${await this.l.api.getDisplay(res.userId)} ${this.l.u.timeSince(res.ms, 1, true)} ago:${res.action ? ' /me' : ''} ${res.message}`
-    }
-    return this.l.insertAtUser(`Logger returned an invalid type: ${res.type}`, extra)
-  }
-
   public async callIndex(channelId: number, userId: number, params: any, extra: Extra) {
     const [index]: [number] = params
     const targetId = userId
@@ -64,11 +49,13 @@ export class Instance implements PluginInstance {
   }
 
   public async callUser(channelId: number, userId: number, params: any, extra: Extra) {
-    const [targetId, _index]: [number | undefined, number | undefined] = params
+    const [_targetId, _index]: [number | undefined, number | undefined] = params
 
-    const index = _index === undefined ? this.l.u.randomInt(0, this.log.eventCount(channelId, userId, 'chat') || 0) : _index
+    const targetId = _targetId || userId
 
-    const res = await this.log.getSmartEvent(channelId, targetId || userId, 'chat', index)
+    const index = _index === undefined ? this.l.u.randomInt(0, this.log.eventCount(channelId, targetId, 'chat') || 0) : _index
+
+    const res = await this.log.getSmartEvent(channelId, targetId, 'chat', index)
     if (!res) return this.l.insertAtUser(`${extra.words[1]} has no logged messages`, extra)
     if (res.type === events.chat) {
       return `${await this.l.api.getDisplay(res.userId)} ${this.l.u.timeSince(res.ms, 1, true)} ago:${res.action ? ' /me' : ''} ${res.message}`
